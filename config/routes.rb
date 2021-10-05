6e9519f99ce94859502ac0eef8dc86ba8940b764
Melissa Guachun <1212malka@gmail.com>
2021-10-05 15:02:47 -0400

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'concerts/index'
-  get 'concerts/create'
-  get 'concerts/new'
-  get 'concerts/show'
-  get 'concerts/destroy'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  resources :users
+  resources :concerts
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
