0d326168d20bfa68b1704d16c3dc0a336ae7427f
Melissa Guachun <1212malka@gmail.com>
2021-10-05 22:14:33 -0400

    added session routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   resources :users
   resources :concerts
 
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
