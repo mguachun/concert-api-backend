3a250179982e6c46106b340d6f3b7dd3cb550ecb
Melissa Guachun <1212malka@gmail.com>
2021-10-05 22:14:22 -0400

    got rid of @'s

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,33 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :destroy]
   def index
-    @users = User.all
-    render json: @users
+    users = User.all
+    render json: users
   end
 
   def create
-    @user = User.new(email: params[:email], password: params[:password])
-    if @user.save
-      render json @user 
+    user = User.new(email: params[:email], password: params[:password])
+    if user.save
+      render json user 
     else 
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
-  def signin
-    @user = User.find_by(email: params[:email])
 
-    if @user && @user.authenticate(params[:password])
-      render json: @user
+  def show
+    render json: @users,
+    except: [:created_at, :updated_at]
+  end 
+
+
+  def signin
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      render json: user
     else
-      if @user
+      if user
       render json: "Invalid Password", status: :unprocessable_entity
     else
       render json: "Email Address Not Found", status: :unprocessable_entity
@@ -28,8 +35,6 @@ class UsersController < ApplicationController
   end
 end
    
-
-
   private
   def set_user
     @user= User.find(params[:id])
