2d917e86be9ea2d0a5b916d0a71d19b5e1126384
Melissa Guachun <1212malka@gmail.com>
2021-10-20 15:27:21 -0400

    deleted users

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,5 @@ Rails.application.routes.draw do
 
   resources :concerts
 
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
-
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
